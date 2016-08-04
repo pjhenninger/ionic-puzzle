b554eb6ac312d1280b2286e5fcec6834a27c3394
Preston <preston.henninger@us.ibm.com>
2016-08-04 16:17:09 -0500

    structuring

--- public/js/app.js
+++ public/js/app.js
@@ -6,104 +6,117 @@
 angular.module('starter', ['ionic'])
 
 
-  .run(function ($ionicPlatform) {
-    var board = $("#puzzle-board");
-    var puzzlePieces = $("#puzzle-board-pieces");
-    var selectedPiece;
-    var animating = false;
-    var pieceSize = 56;
+    .run(function($ionicPlatform) {
+        var board = $("#puzzle-board");
+        var puzzlePieces = $("#puzzle-board-pieces");
+        var selectedPiece;
+        var animating = false;
+        var pieceSize = 56;
 
-    $ionicPlatform.ready(function () {
+        $ionicPlatform.ready(function() {
 
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-        // Don't remove this line unless you know what you are doing. It stops the viewport
-        // from snapping when text inputs are focused. Ionic handles this internally for
-        // a much nicer keyboard experience.
-        cordova.plugins.Keyboard.disableScroll(true);
-      }
-      if (window.StatusBar) {
-        StatusBar.styleDefault();
-      }
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
 
-      $.ajax({
-        url: 'http://localhost:3000/createPuzzle',
-        type: 'GET',
-        data: '', // or $('#myform').serializeArray()
-        success: function (data) { loadPuzzle(data) }
-      });
-    });
+            $.ajax({
+                url: 'http://localhost:3000/createPuzzle',
+                type: 'GET',
+                data: '', // or $('#myform').serializeArray()
+                success: function(data) { loadPuzzle(data) }
+            });
+        });        
 
+        function loadPuzzle(colors) {
+            $.each(JSON.parse(colors), function(i) {
+                $.each(this, function(j) {
+                    createPuzzlePiece(this.Color, i, j);
+                })
+            });
+        }
 
-    $(document).mouseup(function () {
-      selectedPiece = undefined;
-    });
+        function createPuzzlePiece(color, rowIndex, columnIndex) {
+            var piece = document.createElement("div");
+            var img = document.createElement("img");
+            img.draggable = false;
+            piece.draggable = false;
+            img.src = "img/" + color + ".png";
+            piece.style.top = (rowIndex * pieceSize).toString() + "px";
+            piece.style.left = (columnIndex * pieceSize).toString() + "px";
+            piece.className = "puzzle-piece";
+            piece.id = rowIndex + '|' + columnIndex;
+            piece.appendChild(img);
+            puzzlePieces.append(piece);
 
-    puzzlePieces.mouseleave(function () {
-      selectedPiece = undefined;
-    });
+            $(piece).mousedown(function() {
+                // Show start dragged position of image.
+                selectedPiece = $(this);
+            });
+            $(piece).mouseenter(function() {
+                if (selectedPiece && doPiecesTouch(selectedPiece, $(this), pieceSize)) {
+                    selectedPiece.swapWith($(this), function(){
+                      checkForMatches();
+                    });
 
-    jQuery.fn.swapWith = function (to) {
-      if (animating) {
-        return;
-      }
-      animating = true;
-      thisPos = $(this).position();
-      toPos = $(to).position();
-      $.when($(this).animate({
-        top: toPos.top,
-        left: toPos.left
-      }, 300),
-        $(to).animate({
-          top: thisPos.top,
-          left: thisPos.left
-        }, 300)).done(function () {
-          animating = false;
+                    selectedPiece = undefined;
+                }
+            });
+        }
+
+        function checkForMatches(){
+          
+        }
+
+        $(document).mouseup(function() {
+            selectedPiece = undefined;
         });
 
+        puzzlePieces.mouseleave(function() {
+            selectedPiece = undefined;
+        });
 
-    };
+        jQuery.fn.swapWith = function(to, callback) {          
 
-    function loadPuzzle(colors) {      
-      $.each(JSON.parse(colors), function (i) {
-        $.each(this, function (j) {
-          var piece = document.createElement("div");
-          var img = document.createElement("img");
-          img.draggable = false;
-          piece.draggable = false;
-          img.src = "img/" + this.Color + ".png";
-          piece.style.top = (i * pieceSize).toString() + "px";
-          piece.style.left = (j * pieceSize).toString() + "px";
-          piece.className = "puzzle-piece";
-          piece.appendChild(img);
-          puzzlePieces.append(piece);
-
-          $(piece).mousedown(function () {
-            // Show start dragged position of image.
-            selectedPiece = $(this);
-          });
-          $(piece).mouseenter(function () {
-            if (selectedPiece && doPiecesTouch(selectedPiece, $(this), pieceSize)) {
-              selectedPiece.swapWith($(this));
-              selectedPiece = undefined;
+            if (animating) {
+                return;
             }
-          });
-        })
-      });
+            animating = true;
+            thisPos = $(this).position();
+            toPos = $(to).position();
+            $.when($(this).animate({
+                top: toPos.top,
+                left: toPos.left
+            }, 300),
+                $(to).animate({
+                    top: thisPos.top,
+                    left: thisPos.left
+                }, 300)).done(function() {
+                    animating = false;
+                    if(callback){
+                      callback();                      
+                    }                    
+                });
 
-    }
 
-  })
+        };
+    })
 
-  function doPiecesTouch(piece1, piece2, pieceSize){
+function doPiecesTouch(piece1, piece2, pieceSize) {
     var fromPos = piece1.position();
     var toPos = piece2.position();
-    
+
     return (toPos.top === fromPos.top || toPos.left === fromPos.left);
-  }
+}
 
 
 
