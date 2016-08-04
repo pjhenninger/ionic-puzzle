def75d299109376af879e94679efc00ecee83579
Preston <preston.henninger@us.ibm.com>
2016-08-04 15:44:21 -0500

    puzzle color validation

--- components/PuzzleBoard.ts
+++ components/PuzzleBoard.ts
@@ -1,19 +1,44 @@
-import * as p from './PuzzlePiece'; 
+import * as p from './PuzzlePiece';
 
-export class PuzzleBoard{
-    public PuzzlePieces : p.PuzzlePiece[][];
+export class PuzzleBoard {
+    public PuzzlePieces: p.PuzzlePiece[][];
+    private rowCount = 8;
+    private columnCount = 8;
 
-    constructor(){
+    constructor() {
         this.PuzzlePieces = [];
-         for(var i: number = 0; i < 8; i++) {       
-             this.PuzzlePieces[i] = [];     
-            for(var j: number = 0; j< 8; j++) {
-                this.PuzzlePieces[i][j] = new p.PuzzlePiece(p.getRandomColor());
+        for (var i: number = 0; i < this.rowCount; i++) {
+            this.PuzzlePieces[i] = [];
+            for (var j: number = 0; j < this.columnCount; j++) {
+                this.PuzzlePieces[i][j] = new p.PuzzlePiece(this.getRandomValidColor(i, j));
             }
         }
     }
 
-    public toJSON() : string {
+    public toJSON(): string {
         return JSON.stringify(this.PuzzlePieces);
+    }
+
+    private getRandomValidColor(rowIndex, columnIndex): p.PieceColor {
+        var color = p.getRandomColor();
+
+        if (rowIndex >= 2 && columnIndex < 2) {
+            while (color === this.PuzzlePieces[rowIndex - 1][columnIndex].Color && color === this.PuzzlePieces[rowIndex - 2][columnIndex].Color) {
+                color = p.getRandomColor();
+            }
+        }
+        else if (columnIndex >= 2 && rowIndex < 2) {
+            while (color === this.PuzzlePieces[rowIndex][columnIndex - 1].Color && color === this.PuzzlePieces[rowIndex][columnIndex - 2].Color) {
+                color = p.getRandomColor();
+            }
+        }
+        else if (rowIndex >= 2 && columnIndex >= 2) {
+            while ((color === this.PuzzlePieces[rowIndex - 1][columnIndex].Color && color === this.PuzzlePieces[rowIndex - 2][columnIndex].Color) ||
+                (color === this.PuzzlePieces[rowIndex][columnIndex - 1].Color && color === this.PuzzlePieces[rowIndex][columnIndex - 2].Color)) {
+                color = p.getRandomColor();
+            }
+        }
+
+        return color;
     }
 }
--- dist/components/PuzzleBoard.js
+++ dist/components/PuzzleBoard.js
@@ -2,16 +2,38 @@
 const p = require('./PuzzlePiece');
 class PuzzleBoard {
     constructor() {
+        this.rowCount = 8;
+        this.columnCount = 8;
         this.PuzzlePieces = [];
-        for (var i = 0; i < 8; i++) {
+        for (var i = 0; i < this.rowCount; i++) {
             this.PuzzlePieces[i] = [];
-            for (var j = 0; j < 8; j++) {
-                this.PuzzlePieces[i][j] = new p.PuzzlePiece(p.getRandomColor());
+            for (var j = 0; j < this.columnCount; j++) {
+                this.PuzzlePieces[i][j] = new p.PuzzlePiece(this.getRandomValidColor(i, j));
             }
         }
     }
     toJSON() {
         return JSON.stringify(this.PuzzlePieces);
+    }
+    getRandomValidColor(rowIndex, columnIndex) {
+        var color = p.getRandomColor();
+        if (rowIndex >= 2 && columnIndex < 2) {
+            while (color === this.PuzzlePieces[rowIndex - 1][columnIndex].Color && color === this.PuzzlePieces[rowIndex - 2][columnIndex].Color) {
+                color = p.getRandomColor();
+            }
+        }
+        else if (columnIndex >= 2 && rowIndex < 2) {
+            while (color === this.PuzzlePieces[rowIndex][columnIndex - 1].Color && color === this.PuzzlePieces[rowIndex][columnIndex - 2].Color) {
+                color = p.getRandomColor();
+            }
+        }
+        else if (rowIndex >= 2 && columnIndex >= 2) {
+            while ((color === this.PuzzlePieces[rowIndex - 1][columnIndex].Color && color === this.PuzzlePieces[rowIndex - 2][columnIndex].Color) ||
+                (color === this.PuzzlePieces[rowIndex][columnIndex - 1].Color && color === this.PuzzlePieces[rowIndex][columnIndex - 2].Color)) {
+                color = p.getRandomColor();
+            }
+        }
+        return color;
     }
 }
 exports.PuzzleBoard = PuzzleBoard;
