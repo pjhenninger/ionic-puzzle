32cc660646f03bca030046fc32a67eb0e9a5daee
Preston <preston.henninger@us.ibm.com>
2016-08-05 16:12:25 -0500

    switching to angular

--- public/js/puzzle-client.js
+++ public/js/puzzle-client.js
@@ -3,17 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+app = angular.module('starter', ['ionic'])
+    .run(function ($ionicPlatform) {
 
-
-    .run(function($ionicPlatform) {
-        var board = $("#puzzle-board");
-        var puzzlePieces = $("#puzzle-board-pieces");
-        var selectedPiece;
-        var animating = false;
-        var pieceSize = 56;
-
-        $ionicPlatform.ready(function() {
+        $ionicPlatform.ready(function () {
 
             if (window.cordova && window.cordova.plugins.Keyboard) {
                 // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -28,94 +21,130 @@ angular.module('starter', ['ionic'])
             if (window.StatusBar) {
                 StatusBar.styleDefault();
             }
-
-            $.ajax({
-                url: '/api/createPuzzle',
-                type: 'GET',
-                data: '', // or $('#myform').serializeArray()
-                success: function(data) { loadPuzzle(data) }
-            });
-        });        
-
-        function loadPuzzle(colors) {
-            $.each(JSON.parse(colors), function(i) {
-                $.each(this, function(j) {
-                    createPuzzlePiece(this.Color, i, j);
-                })
-            });
-        }
-
-        function createPuzzlePiece(color, rowIndex, columnIndex) {
-            var piece = document.createElement("div");
-            var img = document.createElement("img");
-            img.draggable = false;
-            piece.draggable = false;
-            img.src = "img/" + color + ".png";
-            piece.style.top = (rowIndex * pieceSize).toString() + "px";
-            piece.style.left = (columnIndex * pieceSize).toString() + "px";
-            piece.className = "puzzle-piece";
-            piece.id = rowIndex + '|' + columnIndex;
-            piece.appendChild(img);
-            puzzlePieces.append(piece);
-
-            $(piece).mousedown(function() {
-                // Show start dragged position of image.
-                selectedPiece = $(this);
-            });
-            $(piece).mouseenter(function() {
-                if (selectedPiece && doPiecesTouch(selectedPiece, $(this), pieceSize)) {
-                    selectedPiece.swapWith($(this), function(){
-                      checkForMatches();
-                    });
-
-                    selectedPiece = undefined;
-                }
-            });
-        }
-
-        function checkForMatches(){
-          
-        }
-
-        $(document).mouseup(function() {
-            selectedPiece = undefined;
         });
 
-        puzzlePieces.mouseleave(function() {
-            selectedPiece = undefined;
+
+    });
+
+app.controller('myCtrl', function ($scope, $interval, $http) {
+    $scope.puzzlePieces = [];
+    $scope.pieceSize = 56;
+    var puzzlePiecesJQ = $("#puzzle-board-pieces");
+    var selectedPiece;
+    var animating = false;
+
+    $.ajax({
+        url: '/api/createPuzzle',
+        type: 'GET',
+        data: '', // or $('#myform').serializeArray()
+        success: function (data) { loadPuzzle(data) }
+    });
+
+    function loadPuzzle(colors) {
+        $.each(JSON.parse(colors), function (i) {
+            $scope.puzzlePieces[i] = [];
+            $.each(this, function (j) {
+                $scope.puzzlePieces[i][j] = this;
+                //createPuzzlePiece(this.Color, i, j);
+            })
         });
+    }
 
-        jQuery.fn.swapWith = function(to, callback) {          
+    function createPuzzlePiece(color, rowIndex, columnIndex) {
+        var piece = document.createElement("div");
+        var img = document.createElement("img");
+        img.draggable = false;
+        piece.draggable = false;
+        img.src = "img/" + color + ".png";
+        piece.style.top = (rowIndex * pieceSize).toString() + "px";
+        piece.style.left = (columnIndex * pieceSize).toString() + "px";
+        piece.className = "puzzle-piece";
+        piece.id = rowIndex + '|' + columnIndex;
+        piece.appendChild(img);
+        puzzlePiecesJQ.append(piece);
 
-            if (animating) {
-                return;
-            }
-            animating = true;
-            thisPos = $(this).position();
-            toPos = $(to).position();
-            $.when($(this).animate({
-                top: toPos.top,
-                left: toPos.left
-            }, 300),
-                $(to).animate({
-                    top: thisPos.top,
-                    left: thisPos.left
-                }, 300)).done(function() {
-                    animating = false;
-                    if(callback){
-                      callback();                      
-                    }                    
+        $(piece).mousedown(function () {
+            // Show start dragged position of image.
+            selectedPiece = $(this);
+        });
+        $(piece).mouseenter(function () {
+            if (selectedPiece && doPiecesTouch(selectedPiece, $(this), $scope.pieceSize)) {
+                selectedPiece.swapWith($(this), function () {
+                    checkForSpecificMatches(selectedPiece, $(this));
                 });
 
+                selectedPiece = undefined;
+            }
+        });
+    }
 
-        };
-    })
+    function checkForSpecificMatches(piece1, piece2) {
+
+    }
+
+    function checkForAllMatches() {
+
+    }
+
+    $(document).mouseup(function () {
+        selectedPiece = undefined;
+    });
+
+    puzzlePiecesJQ.mouseleave(function () {
+        selectedPiece = undefined;
+    });
+
+    $scope.pieceMouseDown = function () {
+        // Show start dragged position of image.
+        selectedPiece = $(this);
+    }
+
+    $scope.pieceMouseEnter = function () {
+        if (selectedPiece && doPiecesTouch(selectedPiece, $(this), $scope.pieceSize)) {
+            selectedPiece.swapWith($(this), function () {
+                checkForSpecificMatches(selectedPiece, $(this));
+            });
+
+            selectedPiece = undefined;
+        }
+    }
+
+    $scope.getAbsPos = function (rowIndex, columnIndex) {        
+        return { "top": (rowIndex * $scope.pieceSize + 'px'),
+                 "left": (columnIndex * $scope.pieceSize + 'px') };
+    }
+
+    jQuery.fn.swapWith = function (to, callback) {
+        var z = $scope.puzzlePieces[0][1];
+        var b = $scope.puzzlePieces[0][0];        
+        $scope.puzzlePieces[0][0] = z;
+        $scope.puzzlePieces[0][1] = b;
+        if (animating) {
+            return;
+        }
+        // animating = true;
+        // thisPos = $(this).position();
+        // toPos = $(to).position();
+        // $.when($(this).animate({
+        //     top: toPos.top,
+        //     left: toPos.left
+        // }, 300),
+        //     $(to).animate({
+        //         top: thisPos.top,
+        //         left: thisPos.left
+        //     }, 300)).done(function () {
+        //         animating = false;
+        //         if (callback) {
+        //             callback();
+        //         }
+        //     });
+
+
+    };
+});
 
 function doPiecesTouch(piece1, piece2, pieceSize) {
-    var fromPos = piece1.position();
-    var toPos = piece2.position();
-
-    return (toPos.top === fromPos.top || toPos.left === fromPos.left);
+    return (piece1.prop('top') === piece2.prop('top') || piece1.prop('left') === piece2.prop('left'));
 }
 
 
